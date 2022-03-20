63dc368162f059390b29766b29decb758b71b1f2
RyanSKJ <57385867+RyanSKJ@users.noreply.github.com>
2022-03-20 17:05:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -212,7 +212,7 @@ if __name__ == '__main__':
     chrome_options = Options()
     chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument("--start-maximized")
-    #chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
  
     driver_pjs = webdriver.Chrome(
         options=chrome_options,
